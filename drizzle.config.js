415caf114f69b0e05287e67ae1f78a369d5c7542
Michalis Mavromatakis <m.mavromatakis@flexcar.gr>
2026-03-19 14:33:04 +0200

    Set Up Neo Postgres with Drizzle

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+// Drizzle: Used to connect to the database
+import 'dotenv/config';
+
+export default {
+  schema: './src/models/*.js',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+};
