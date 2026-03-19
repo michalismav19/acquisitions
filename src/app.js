fac7399fc7ef7228169b18cdf4d33a7dce2f44cf
Michalis Mavromatakis <m.mavromatakis@flexcar.gr>
2026-03-19 13:51:43 +0200

    Add linter and prettier

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 // Setting Up Express application server with the right middleware
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World');
 });
 
 export default app;
