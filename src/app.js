3eb11d31e85946029b541f34a42ca15f68f2c21e
Michalis Mavromatakis <m.mavromatakis@flexcar.gr>
2026-03-26 18:26:31 +0200

    Add logger and middlewares

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
 // Setting Up Express application server with the right middleware
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+// helmet is a middleware
+// Help secure Express apps with various HTTP headers
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// morgan is a middleware
+// HTTP request logger middleware for node.js
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World');
+  logger.info('Hello World!');
+  res.status(200).send('Hello World!');
 });
 
 export default app;
